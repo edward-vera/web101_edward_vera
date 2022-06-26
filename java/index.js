7f737a00ad14cc2a6acf5b32b122d3350fa28075
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-06-26 14:24:28 -0500

    outline finished

--- java/index.js
+++ java/index.js
@@ -5,4 +5,14 @@ const header = document.querySelector('.header.container');
 hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 800){
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent'; 
+    }
+
 })
